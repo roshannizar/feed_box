26d76e8026f4f21f40d60f0b61c2bc9828e4ef57
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-30 17:35:55 +0530

    Create follower_list_model.dart

---
+++ lib/models/follower_list_model.dart
@@ -0,0 +1,7 @@
+class FollowerListModel {
+  final String friendName;
+  final String friendUid;
+  final String messageId;
+
+  FollowerListModel({this.friendName,this.friendUid,this.messageId});
+}
