8d134be716151941d5ad80700bdd2b489239d0b7
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-30 17:37:28 +0530

    Create like_tile.dart

---
+++ lib/screens/feeds/likes/like_tile.dart
@@ -0,0 +1,34 @@
+import 'package:feed_box/models/like_model.dart';
+import 'package:feed_box/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LikeTile extends StatefulWidget {
+  @override
+  _LikeTileState createState() => _LikeTileState();
+}
+
+class _LikeTileState extends State<LikeTile> {
+  bool liked=false;
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    final likesCount = Provider.of<List<LikeModel>>(context) ?? [];
+
+    likesCount.forEach((f) {
+      if (f.profile == user.uid) {
+        liked = true;
+      }
+    });
+
+    return FlatButton.icon(
+        onPressed: () {},
+        icon: Icon(
+          liked ? Icons.favorite : Icons.favorite_border,
+          size: 15,
+          color: liked ? Colors.red : Colors.black,
+        ),
+        label: Text('${likesCount.length}'));
+  }
+}
