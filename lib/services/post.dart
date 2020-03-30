05ae5051ed8f5031a3fdd7ac16ee0853786d9caf
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-30 17:39:14 +0530

    Update post.dart

--- lib/services/post.dart
+++ lib/services/post.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:feed_box/models/comments_model.dart';
+import 'package:feed_box/models/like_model.dart';
 import 'package:feed_box/models/post.dart';
 
 class PostService {
@@ -25,7 +27,8 @@ class PostService {
           description: doc.data['description'] ?? '',
           date: doc.data['date'] ?? '',
           postUrl: doc.data['postUrl'] ?? '',
-          uid: doc.data['uid'] ?? '');
+          uid: doc.data['uid'] ?? '',
+          documentId: doc.documentID ?? '');
     }).toList();
   }
 
@@ -40,8 +43,37 @@ class PostService {
     }).toList();
   }
 
+  List<LikeModel> _likes(QuerySnapshot snapshot) {
+    return snapshot.documents.map((f) {
+      return LikeModel(profile: f.data['profile'] ?? '');
+    }).toList();
+  }
+
+  List<CommentModel> _comments(QuerySnapshot snapshot) {
+    return snapshot.documents.map((f) {
+      return CommentModel(
+          profile: f.data['profile'], content: f.data['comments']);
+    }).toList();
+  }
+
   Stream<List<Post>> get posts {
     return postCollection.snapshots().map(_postsListCollection);
+  }
+
+  Stream<List<LikeModel>> get likes {
+    return postCollection
+        .document(uid)
+        .collection('likes')
+        .snapshots()
+        .map(_likes);
+  }
+
+  Stream<List<CommentModel>> get getComments {
+    return postCollection
+        .document(uid)
+        .collection('comments')
+        .snapshots()
+        .map(_comments);
   }
 
   Stream<List<Post>> get myPosts {
