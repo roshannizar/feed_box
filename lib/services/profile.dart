13ffa1d766803ef2504349c73f57950f107a54bb
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-30 17:39:18 +0530

    Update profile.dart

--- lib/services/profile.dart
+++ lib/services/profile.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:feed_box/models/follower_list_model.dart';
 import 'package:feed_box/models/profile.dart';
 
 class ProfileService {
   final String uid;
+  String profileUid;
 
   ProfileService({this.uid});
 
@@ -10,32 +12,54 @@ class ProfileService {
       Firestore.instance.collection('profile');
 
   Future updateProfile(String fullname, String email) async {
-    return await profileCollection
-        .document(uid)
-        .setData({'uid': uid, 'fullname': fullname, 'email': email});
+    try {
+      return await profileCollection
+          .document(uid)
+          .setData({'uid': uid, 'fullname': fullname, 'email': email});
+    } catch (e) {
+      print(e);
+    }
   }
 
-  Profile _profileData(DocumentSnapshot snapshot) {
-    return Profile(
+  ProfileModel _profileData(DocumentSnapshot snapshot) {
+
+    return ProfileModel(
         uid: uid,
         fullname: snapshot.data['fullname'],
         email: snapshot.data['email']);
   }
 
-  List<Profile> _profileList(QuerySnapshot snapshot) {
+  List<ProfileModel> _profileList(QuerySnapshot snapshot) {
     return snapshot.documents.map((doc) {
-      return Profile(
+      return ProfileModel(
           fullname: doc.data['fullname'],
           uid: doc.data['uid'],
           email: doc.data['email']);
     }).toList();
   }
 
-  Stream<Profile> get profileData {
+  List<FollowerListModel> _follower(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return FollowerListModel(
+          friendName: doc.data['friendName'],
+          friendUid: doc.data['friendUid'],
+          messageId: doc.data['messageId']);
+    }).toList();
+  }
+
+  Stream<ProfileModel> get profileData {
     return profileCollection.document(uid).snapshots().map(_profileData);
   }
 
-  Stream<List<Profile>> get allProfile {
+  Stream<List<ProfileModel>> get allProfile {
     return profileCollection.snapshots().map(_profileList);
+  }
+
+  Stream<List<FollowerListModel>> get getFollower {
+    return profileCollection
+        .document(profileUid)
+        .collection('followers')
+        .snapshots()
+        .map(_follower);
   }
 }
