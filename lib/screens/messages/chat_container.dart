b870802fc0beaabd4b8a3a0ec8fceb6615e02014
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-30 17:38:47 +0530

    Create chat_container.dart

---
+++ lib/screens/messages/chat_container.dart
@@ -0,0 +1,47 @@
+import 'package:feed_box/models/chat_model.dart';
+import 'package:feed_box/models/user.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ChatContainer extends StatefulWidget {
+  @override
+  _ChatContainerState createState() => _ChatContainerState();
+}
+
+class _ChatContainerState extends State<ChatContainer> {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    final chat = Provider.of<List<ChatModel>>(context) ?? [];
+
+    return ListView.builder(
+      reverse: true,
+      itemCount: chat.length,
+      itemBuilder: (context, index) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            user.uid == chat[index].from
+                ? Container(
+                    padding: const EdgeInsets.all(10),
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                        padding: const EdgeInsets.all(10),
+                        color: Colors.amber,
+                        child: Text('${chat[index].content}')),
+                  )
+                : Container(
+                    padding: const EdgeInsets.all(10),
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                        padding: const EdgeInsets.all(10),
+                        color: Colors.amber,
+                        child: Text('${chat[index].content}')),
+                  )
+          ],
+        );
+      },
+    );
+  }
+}
