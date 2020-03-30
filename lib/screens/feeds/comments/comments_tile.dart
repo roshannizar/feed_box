1478393d57e28a5476414b4c85f8e780a5eb5e2b
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-30 17:37:22 +0530

    Create comments_tile.dart

---
+++ lib/screens/feeds/comments/comments_tile.dart
@@ -0,0 +1,19 @@
+import 'package:feed_box/models/comments_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CommentsTile extends StatefulWidget {
+  @override
+  _CommentsTileState createState() => _CommentsTileState();
+}
+
+class _CommentsTileState extends State<CommentsTile> {
+  @override
+  Widget build(BuildContext context) {
+    final comments = Provider.of<List<CommentModel>>(context) ?? [];
+    return FlatButton.icon(
+        onPressed: () {},
+        icon: Icon(Icons.comment, size: 15),
+        label: Text('${comments.length}'));
+  }
+}
