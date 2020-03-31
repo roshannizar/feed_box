e529228a79176b9f1883bcff8856834033d64ad6
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-31 10:35:13 +0530

    updated bottomsheet

--- lib/screens/feeds/comments/comments_tile.dart
+++ lib/screens/feeds/comments/comments_tile.dart
@@ -2,17 +2,38 @@ import 'package:feed_box/models/comments_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'comments_container.dart';
+
 class CommentsTile extends StatefulWidget {
+  final String docid;
+  CommentsTile({this.docid});
   @override
   _CommentsTileState createState() => _CommentsTileState();
 }
 
 class _CommentsTileState extends State<CommentsTile> {
+
+  void _showCommentsPanel() {
+    showModalBottomSheet(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20))),
+        isScrollControlled: true,
+        context: context,
+        builder: (context) {
+          return FractionallySizedBox(
+            heightFactor: 0.9,
+            child: CommentsContainer(docid: widget.docid),
+          );
+        });
+  }
+  
   @override
   Widget build(BuildContext context) {
     final comments = Provider.of<List<CommentModel>>(context) ?? [];
+
     return FlatButton.icon(
-        onPressed: () {},
+        onPressed: () {
+          _showCommentsPanel();
+        },
         icon: Icon(Icons.comment, size: 15),
         label: Text(comments.length == 0 ? '' : '${comments.length}'));
   }
