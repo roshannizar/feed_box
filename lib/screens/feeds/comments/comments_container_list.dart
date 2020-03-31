79493dc6f09d5ca14f624f8668671209e3df43e0
Roshan Nizar <IT17048774@my.sliit.lk>
2020-03-31 10:34:50 +0530

    Create comments_container_list.dart

---
+++ lib/screens/feeds/comments/comments_container_list.dart
@@ -0,0 +1,27 @@
+import 'package:feed_box/models/comments_model.dart';
+import 'package:feed_box/shared/loading.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'comments_container_tile.dart';
+
+class CommentsContainerList extends StatefulWidget {
+  @override
+  _CommentsContainerListState createState() => _CommentsContainerListState();
+}
+
+class _CommentsContainerListState extends State<CommentsContainerList> {
+  @override
+  Widget build(BuildContext context) {
+
+    final comments = Provider.of<List<CommentModel>>(context) ?? [];
+
+    return comments.length == 0 ? Loading() :  ListView.builder(
+            shrinkWrap: true,
+            itemCount: comments.length,
+            itemBuilder: (context, index) {
+              return CommentsContainerTile(comment: comments[index]);
+            },
+          );
+  }
+}
