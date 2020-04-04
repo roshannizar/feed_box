434be5f7f5baefaaca2400fd4aecb4d5c301a178
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-04 08:19:53 +0530

    Update post_tile.dart

--- lib/screens/postpanel/post_tile.dart
+++ lib/screens/postpanel/post_tile.dart
@@ -1,5 +1,4 @@
 import 'package:feed_box/models/post_model.dart';
-import 'package:feed_box/shared/profile_header.dart';
 import 'package:flutter/material.dart';
 
 class PostTile extends StatelessWidget {
