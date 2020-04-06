55ac26bd6581d5e08ffe5c043e2b4c6d53996c8d
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-06 15:35:07 +0530

    Update share.dart

--- lib/screens/feeds/share/share.dart
+++ lib/screens/feeds/share/share.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:share/share.dart';
 
 class ShareContainer extends StatelessWidget {
-  final String postUrl,description;
-  ShareContainer({this.postUrl,this.description});
+  final String postUrl, description;
+  ShareContainer({this.postUrl, this.description});
   @override
   Widget build(BuildContext context) {
     return FlatButton.icon(
-                      onPressed: () {
-                        Share.share('$postUrl',
-                            subject: '$description');
-                      },
-                      label: Text('Share'),
-                      icon: Icon(Icons.share, size: 15));
+        onPressed: () {
+          Share.share('$postUrl', subject: '$description');
+        },
+        label: Text('Share'),
+        icon: Icon(Icons.share, size: 15));
   }
 }
