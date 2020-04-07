c1c9351f9541b10d50f8a1e9a313d2f71c65a87c
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-07 18:11:56 +0530

    Create activity_model.dart

---
+++ lib/models/activity_model.dart
@@ -0,0 +1,9 @@
+class ActivityModel {
+  final String uid;
+  final String receiverUid;
+  final String date;
+  final String type;
+  final bool status;
+
+  ActivityModel({this.uid, this.receiverUid, this.date, this.type,this.status});
+}
