2972e4a55cf3247264e8b2417ed81df6e7bcd23f
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-07 18:12:06 +0530

    Create friend_request.dart

---
+++ lib/screens/activity/request/friend_request.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FriendRequest extends StatefulWidget {
+  @override
+  _FriendRequestState createState() => _FriendRequestState();
+}
+
+class _FriendRequestState extends State<FriendRequest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
