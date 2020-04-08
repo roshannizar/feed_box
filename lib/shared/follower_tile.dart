f970f1b8641445ff2e5f2d99737c8e79263e5e08
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-08 14:55:20 +0530

    Create follower_tile.dart

---
+++ lib/shared/follower_tile.dart
@@ -0,0 +1,22 @@
+import 'package:feed_box/models/follower_list_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class FollowerTile extends StatefulWidget {
+  final String type;
+  FollowerTile({this.type});
+  @override
+  _FollowerTileState createState() => _FollowerTileState();
+}
+
+class _FollowerTileState extends State<FollowerTile> {
+  @override
+  Widget build(BuildContext context) {
+    final followerCount = Provider.of<List<FollowerListModel>>(context) ?? [];
+
+    return FlatButton(
+      child: Text('${widget.type} ${followerCount.length}'),
+      onPressed: () {},
+    );
+  }
+}
