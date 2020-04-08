40018cab2cfce20b5e986e0f15f6749ebf27b488
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-08 14:55:34 +0530

    Update search_tile.dart

--- lib/screens/search/search_tile.dart
+++ lib/screens/search/search_tile.dart
@@ -1,28 +1,46 @@
+import 'package:feed_box/models/follower_list_model.dart';
 import 'package:feed_box/models/profile_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SearchTile extends StatelessWidget {
-  final ProfileModel profile;
+class SearchTile extends StatefulWidget {
+    final ProfileModel profile;
   SearchTile({this.profile});
 
   @override
+  _SearchTileState createState() => _SearchTileState();
+}
+
+class _SearchTileState extends State<SearchTile> {
+  bool following=false;
+  String followerId;
+  @override
   Widget build(BuildContext context) {
-    return Container(
+
+    final followerList = Provider.of<List<FollowerListModel>>(context) ?? [];
+
+    followerList.forEach((f) {
+      if (f.friendUid == widget.profile.uid) {
+        following = true;
+        followerId = f.friendUid;
+      }
+    });
+    return  Container(
       child: Card(
         child: ListTile(
           leading: CircleAvatar(
-            backgroundImage: NetworkImage('${profile.profileUrl}'),
+            backgroundImage: NetworkImage('${widget.profile.profileUrl}'),
           ),
-          title: Text('${profile.fullname}',
+          title: Text('${widget.profile.fullname}',
               style: TextStyle(fontWeight: FontWeight.bold)),
-          subtitle: Text('@${profile.email}'),
+          subtitle: Text('@${widget.profile.email}'),
           trailing: FlatButton.icon(
-              color: Colors.blue,
+              color: following? Colors.white:Colors.blue,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20)),
               onPressed: () {},
-              icon: Icon(Icons.add, color: Colors.white),
-              label: Text('Follow', style: TextStyle(color: Colors.white))),
+              icon: Icon(following? Icons.check:Icons.add, color: following? Colors.blue:Colors.white),
+              label: Text(following? 'Following':'Follow', style: TextStyle(color: following?Colors.blue:Colors.white))),
         ),
       ),
     );
