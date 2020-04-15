c831281e78f205b1a064e2dea616aa02d5b7e367
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-15 18:12:40 +0530

    Update profile_service.dart

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:feed_box/models/activity_model.dart';
 import 'package:feed_box/models/follower_list_model.dart';
 import 'package:feed_box/models/profile_model.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ProfileService {
   final String uid;
@@ -9,31 +12,44 @@ class ProfileService {
   //default profile image
   String profileUrl =
       'https://firebasestorage.googleapis.com/v0/b/feed-box-c1336.appspot.com/o/profile%2Fblank-profile-picture-973460_640.png?alt=media&token=35274574-be73-4e51-a0c4-49ab435803d5';
-
   ProfileService({this.uid});
-
+  final StorageReference firebaseStorage =
+      FirebaseStorage.instance.ref().child('profile');
   final CollectionReference profileCollection =
       Firestore.instance.collection('profile');
 
   //update profile
-  Future updateProfile(String fullname, String email) async {
+  Future updateProfile(String fullname, String email, String bio,
+      String contact, File imageUrl, String url) async {
+    if (imageUrl != null) {
+      StorageUploadTask task =
+          firebaseStorage.child(DateTime.now().toString()).putFile(imageUrl);
+
+      await (task.onComplete).then((s) async {
+        url = await s.ref
+            .getDownloadURL(); //once upload is done url can be extracted
+      });
+    }
+
     await profileCollection.document(uid).setData({
       'uid': uid,
       'fullname': fullname,
       'email': email,
-      'profileUrl': profileUrl
+      'profileUrl': url == null ? profileUrl : url,
+      'bio': bio,
+      'contact': contact
     }).catchError((e) {
       print(e);
     });
 
-    //add a default following method to get all your personal post,
-    //where your uid will be stored and will be easy to retreive the post
-    await profileCollection
-        .document(uid)
-        .collection('following')
-        .add({'friendName': 'Roshan Nizar', 'friendUid': uid}).catchError((e) {
-      print(e);
-    });
+    if (url == null) {
+      //add a default following method to get all your personal post,
+      //where your uid will be stored and will be easy to retreive the post
+      await profileCollection.document(uid).collection('following').add(
+          {'friendName': 'Roshan Nizar', 'friendUid': uid}).catchError((e) {
+        print(e);
+      });
+    }
   }
 
   //activity tracking method
@@ -43,7 +59,7 @@ class ProfileService {
       'titleDirection': activityModel.titleDirection,
       'receiverUid': activityModel.receiverUid,
       'date':
-          '${DateTime.now().year}-${DateTime.now().month.toString().padLeft(2,'0')}-${DateTime.now().day.toString().padLeft(2,'0')} ${DateTime.now().hour.toString().padLeft(2,'0')}:${DateTime.now().minute.toString().padLeft(2,'0')}', //eg: 2020-04-09 16.00
+          '${DateTime.now().year}-${DateTime.now().month.toString().padLeft(2, '0')}-${DateTime.now().day.toString().padLeft(2, '0')} ${DateTime.now().hour.toString().padLeft(2, '0')}:${DateTime.now().minute.toString().padLeft(2, '0')}', //eg: 2020-04-09 16.00
       'title': activityModel.title
     }).catchError((e) {
       print(e);
@@ -53,7 +69,8 @@ class ProfileService {
   //new follwing method
   Future newFollowing(
       FollowerListModel followerListModel, bool following) async {
-    if (following) { //check already following
+    if (following) {
+      //check already following
 
       //update my profile following
       await profileCollection
@@ -75,7 +92,6 @@ class ProfileService {
         print(e);
       });
     } else {
-
       //update my profile following
       await profileCollection
           .document(uid)
@@ -104,7 +120,9 @@ class ProfileService {
         uid: uid,
         fullname: snapshot.data['fullname'],
         email: snapshot.data['email'],
-        profileUrl: snapshot.data['profileUrl']);
+        profileUrl: snapshot.data['profileUrl'],
+        bio: snapshot.data['bio'],
+        contact: snapshot.data['contact']);
   }
 
   //activity list mapping model
@@ -123,10 +141,12 @@ class ProfileService {
   List<ProfileModel> _profileList(QuerySnapshot snapshot) {
     return snapshot.documents.map((doc) {
       return ProfileModel(
-          fullname: doc.data['fullname'],
-          uid: doc.data['uid'],
-          email: doc.data['email'],
-          profileUrl: doc.data['profileUrl']);
+          fullname: doc.data['fullname'] ?? '',
+          uid: doc.data['uid'] ?? '',
+          email: doc.data['email'] ?? '',
+          profileUrl: doc.data['profileUrl'] ?? '',
+          bio: doc.data['bio'] ?? '',
+          contact: doc.data['contact'] ?? '');
     }).toList();
   }
 
