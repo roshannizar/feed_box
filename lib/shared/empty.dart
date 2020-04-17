1c9113a2125ab1a1d210d68137bcc9d44841851c
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-17 16:00:31 +0530

    Create empty.dart

---
+++ lib/shared/empty.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Empty extends StatefulWidget {
+  final String imageUrl;
+  final String text;
+  Empty({this.imageUrl, this.text});
+  @override
+  _EmptyState createState() => _EmptyState();
+}
+
+class _EmptyState extends State<Empty> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('${widget.imageUrl}', width: 100),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '${widget.text}',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
