54b7fd70acbe345f45b63905ece502ff09056b1b
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-17 16:14:21 +0530

    Update profile_header.dart

--- lib/shared/profile_header.dart
+++ lib/shared/profile_header.dart
@@ -1,6 +1,8 @@
 import 'package:feed_box/models/profile_model.dart';
+import 'package:feed_box/models/user_model.dart';
 import 'package:feed_box/services/profile_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfileHeader extends StatefulWidget {
   final String profile, content, type, title;
@@ -14,6 +16,8 @@ class ProfileHeader extends StatefulWidget {
 class _ProfileHeaderState extends State<ProfileHeader> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserModel>(context);
+
     return StreamBuilder<ProfileModel>(
         stream: ProfileService(uid: widget.profile).profileData,
         builder: (context, snapshot) {
@@ -29,10 +33,12 @@ class _ProfileHeaderState extends State<ProfileHeader> {
                 subtitle: Text('${widget.content}'),
               );
             } else if (widget.type == 'activity') {
-              return Text(widget.titleDirection
-                  ? '${profileModel.fullname} ${widget.title}'
-                  : '${widget.title} ${profileModel.fullname}');
-            } else if(widget.type == 'title'){
+              return widget.profile == user.uid
+                  ? Text("${widget.title}")
+                  : Text(widget.titleDirection
+                      ? '${profileModel.fullname} ${widget.title}'
+                      : '${widget.title} ${profileModel.fullname}');
+            } else if (widget.type == 'title') {
               return Text('${profileModel.fullname}');
             } else {
               return CircleAvatar(
