f472d9be972c405f9e5bce63ffc7dd8dbd11577e
Roshan Nizar <IT17048774@my.sliit.lk>
2020-04-17 16:00:24 +0530

    Update search_list.dart

--- lib/screens/search/search_list.dart
+++ lib/screens/search/search_list.dart
@@ -1,5 +1,6 @@
 import 'package:feed_box/models/profile_model.dart';
 import 'package:feed_box/models/user_model.dart';
+import 'package:feed_box/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:feed_box/screens/search/search_tile.dart';
@@ -15,16 +16,18 @@ class _SearchListState extends State<SearchList> {
     final user = Provider.of<UserModel>(context);
     final profiles = Provider.of<List<ProfileModel>>(context) ?? [];
 
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: profiles.length,
-      itemBuilder: (context, index) {
-        if (profiles[index].uid == user.uid) {
-          return SizedBox(height: 0, width: 0);
-        } else {
-          return SearchTile(profile: profiles[index]);
-        }
-      },
-    );
+    return profiles.length == 0
+        ? Loading()
+        : ListView.builder(
+            shrinkWrap: true,
+            itemCount: profiles.length,
+            itemBuilder: (context, index) {
+              if (profiles[index].uid == user.uid) {
+                return SizedBox(height: 0, width: 0);
+              } else {
+                return SearchTile(profile: profiles[index]);
+              }
+            },
+          );
   }
 }
